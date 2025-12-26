21938ecbc4ef8e061eeabe3f358122493a44a10e
keyontesingleton86-wq <keyontesingleton86@icloud.com>
2025-12-26 18:26:20 -0500

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,7 +1,40 @@
-import subprocess
-import os
-import sys
-
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-subprocess.Popen([sys.executable, "gen.py"])
-subprocess.Popen([sys.executable, "utility.py"])
+{
+  "bot_token": "MTQ1MDcxMjgzODQxODIwMjYyNA.Gut0Ih.dqBWt9VkHBCe9O8iuhO0OnicyWbnFdlPBh2DL",
+  "command_prefix": "$",
+  "allowed_guild_ids": [],
+  "free_channel_ids": [
+    1450598555885506614
+  ],
+  "premium_channel_ids": [
+    1450598555617202339
+  ],
+  "booster_channel_ids": [
+    1450598555885506613
+  ],
+  "restock_channel_id": 1450598555617202337,
+  "restock_role_id": 1450598554748981260,
+  "vouch_channel_id": 1450598555885506616,
+  "log_channel_id": 1450598557705965735,
+  "restock_logs_channel_id": 1450598557705965735,
+  "ban_logs_channel_id": 1450598557957750968,
+  "status_log_channel_id": 1450598557705965735,
+  "restocker_logs_id": null,
+  "status_must_be": "Flow Cloud Ultimate Generator",
+  "status_role_id": 1450598554748981264,
+  "vouch_grace_period_seconds": 120,
+  "free_gen_cooldown_seconds": 1,
+  "premium_gen_cooldown_seconds": 10,
+  "booster_gen_cooldown_seconds": 20,
+  "bot_status": {
+    "type": "playing",
+    "name": "Flow Cloud Ultimate Generator"
+  },
+  "embed_color": "#00FFFF",
+  "embed_footer_text": "Flow Cloud",
+  "embed_thumbnail_url": "https://cdn.discordapp.com/attachments/1450598557957750971/1450705011553337406/IMG_5369.png",
+  "Utility_bot_token": "MTQ1NDI0OTAwNTQ1NzYwODc1NQ.G36-Wq.o7fghG9BrvUy9xIjaht2cnmm8zu_YLlgDMkca",
+  "allowed_user_id": "1383641747913183256",
+  "Premium_Role_Id": "",
+  "Review_channel_id": "",
+  "Drop_Channel_Id": ""
+}
