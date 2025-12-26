96fccac137257df6ebc82c7a5aba2b7617dcf2e8
keyontesingleton86-wq <keyontesingleton86@icloud.com>
2025-12-26 18:12:34 -0500

    Update utility.py

--- utility.py
+++ utility.py
@@ -3,5 +3,5 @@ import os
 import sys
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-subprocess.Popen([sys.executable, "gen.py"], creationflags=subprocess.CREATE_NO_WINDOW)
-subprocess.Popen([sys.executable, "utility.py"], creationflags=subprocess.CREATE_NO_WINDOW)
+subprocess.Popen([sys.executable, "gen.py"])
+subprocess.Popen([sys.executable, "utility.py"])
